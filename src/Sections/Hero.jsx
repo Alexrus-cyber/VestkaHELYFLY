feacb0c13c41299e40565a548ac7f6abd0438033
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2023-01-10 14:33:52 +0300

    Hero section

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,7 +1,31 @@
+import styles from './Hero.module.css'
+
 export const Hero = () => {
     return (
-        <section className="">
+        <section className={styles.hero}>
+            <div className={styles.container}>
+                <div>
+                    <div className={styles.contTitle}>
+                        <p className={styles.title}>Эксклюзивная <b style={{color:"#FC9B09"}}>фотосессия</b> при полете на вертолете</p>
+                    </div>
+                    <div className={styles.containerText}>
+                        <div style={{marginTop:"auto", marginBottom: "auto"}}>
+                            <div className={styles.price}>
+                                <div style={{display:"flex", justifyContent:"start", alignItems: "center",height:50, columnGap: 10, color:"white"}}>
+                                    <div>
+                                        <p style={{fontSize: 20, }}>Стоимость:</p>
+                                        <p style={{fontSize: 35, fontWeight: 800}}>1500p</p>
+                                    </div>
+                                    <p className={styles.percent}>%</p>
+                                </div>
+                            </div>
+                            <button className={styles.button}>Забронировать место</button>
+                        </div>
+                    </div>
+                </div>
 
+
+            </div>
         </section>
     )
 }
